3d5b1ec5e077e48d0e055b307b8e9c5ff035fc7b
Seliverstov Maxim <maxim.web.developer@gmail.com>
2013-11-20 19:35:00 +0400

    Update test.

--- test/controllers/web/admin/books_controller_test.rb
+++ test/controllers/web/admin/books_controller_test.rb
@@ -1,8 +1,30 @@
 require 'test_helper'
 
 class Web::Admin::BooksControllerTest < ActionController::TestCase
+  before do
+  end
+
   test 'should get index' do
     get :index
     assert_response :success
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, id: 1
+    assert_response :success
+  end
+
+  test 'should create' do
+  end
+
+  test 'should update' do
+  end
+
+  test 'should destroy' do
   end
 end
